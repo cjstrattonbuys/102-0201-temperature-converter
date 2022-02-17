05c63307a5d5977a471ef180c48b71850d71e47a
CJ Buys <cjstrattonbuys@gmail.com>
2022-02-17 09:52:31 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var tempCelsius = 10;
+var toFahrenheit = tempCelsius * 1.8 + 32;
+console.log(toFahrenheit);
+
+console.log(
+  `Outside my friend’s house it’s ${tempCelsius}°C, which is ${toFahrenheit}°F.`
+);
